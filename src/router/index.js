15d685273998896fd2129e5a146c8bad7c159d0b
JoshYuJump <josh.yu_8@live.com>
2022-01-07 13:57:55 +0800

    Fixed vue router

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Login from "../views/Login.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
